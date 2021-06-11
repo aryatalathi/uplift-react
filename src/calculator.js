3dc4bf10f8b3bf4e5b2768dca34419f2dac13c78
aryat <aryatalathi24@gmail.com>
2021-06-11 18:47:24 +0530

    simple calculator

---
+++ src/calculator.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { render } from "react-dom";
+
+
+function Calculator() {
+              const [variables,setVariables] = React.useState({
+                  value1:0,
+                  value2:0
+              });
+
+              const [ans,setAns] = React.useState('');
+              function updateVariables(event){
+                  const variable = event.target.name;
+                  const val = event.target.value;
+                  setVariables({...variables,[variable]:Number(val)});
+              }
+              function add(){
+                  setAns(variables.value1+variables.value2);
+              }
+
+              function subtract(){
+                  setAns(variables.value1-variables.value2);
+              }
+
+              function multiply(){
+                  setAns(variables.value1*variables.value2);
+              }
+
+              function divide(){
+                  setAns(variables.value1/variables.value2);
+              }
+              return (
+                  <div className="SIMPLE CALCULATOR">
+                      <h1>SIMPLE CALCULATOR</h1>
+                      <input onChange={updateVariables} type="number" placeholder="First Value" name="value1"/>
+                      <input onChange={updateVariables} type="number" placeholder="Second Value" name="value2"/>
+                      <button onClick={add}>ADDITION</button>
+                      <button onClick={subtract}>SUBTRACTION</button>
+                      <button onClick={multiply}>MULTIPLICATION</button>
+                      <button onClick={divide}>DIVISION</button>
+                      <h1 className="heading">ANSWER IS : {ans}</h1>
+                  </div>
+              )
+}
+export default Calculator; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*ref : codesandbox */
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
